1eccedaa65cffaa99c4741ec8f13cfdbe1be5e1f
Jessica <jess.newman19@gmail.com>
2022-12-05 14:01:10 -0500

    simple CRUD operations added to starter folder

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+app.use(express.json())
+
+//Set up server
+const port = 3000
+app.listen(port, () => { 
+    console.log(`App is running on port ${port}.`)
+})
+
+//Routing
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => { 
+    res.status(200).json({
+        status: 'success', 
+        results: tours.length,
+        data: { 
+            tours: tours
+        }
+    })
+})
+
+app.get('/api/v1/tours/:id', (req, res) => { 
+    const id = req.params.id * 1
+    const tour = tours.find(tour => tour.id === id)
+
+    if (!tour) { 
+        return res.status(404).json({ 
+            status: 'fail', 
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(200).json({ 
+        status: 'success', 
+        data: { 
+            tour
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => { 
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => { 
+        res.status(201).json({
+            status: 'success', 
+            data: { 
+                tour: newTour
+            }
+        })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => { 
+    
+})
